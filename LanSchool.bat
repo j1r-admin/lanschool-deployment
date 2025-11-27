9bbecbe7b94bb9a997466442e5cc4bdb9e7a0632
j1r-admin <john.ralph@bingleygrammar.org>
2025-11-27 10:17:31 +0000

    Refactor LanSchool.bat for improved readability and structure

--- LanSchool.bat
+++ LanSchool.bat
@@ -1,16 +1,25 @@
 @echo off
 REM Set TEACHERMC variable to the teacher's machine name (use uppercase only)
-REM Set CHANNEL variable to the unique room channel number ( numeric value 1 - 16000 )
+REM Set CHANNEL variable to the unique room channel number (numeric value 1 - 16000)
 
 SET TEACHERMC=CLASSROOM-H3
 SET CHANNEL=803
 
+REM Check if this is the teacher's machine
 IF "%COMPUTERNAME%" == "%TEACHERMC%" GOTO :TEACHER
 
 :STUDENT
-If not exist "C:\Program Files (x86)\LanSchool\student.exe" msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Student.msi" /qn ADVANCED_OPTIONS=1 STEALTH_MODE=1 NO_KEYBOARD_MONITORING_MODE=1  NO_INTERNET_MONITORING_MODE=1 CHANNEL=%CHANNEL%
+REM Install LanSchool Student if not already installed
+IF NOT EXIST "C:\Program Files (x86)\LanSchool\student.exe" (
+	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Student.msi" /qn ADVANCED_OPTIONS=1 STEALTH_MODE=1 NO_KEYBOARD_MONITORING_MODE=1 NO_INTERNET_MONITORING_MODE=1 CHANNEL=%CHANNEL%
+	IF %ERRORLEVEL% NEQ 0 echo [%DATE% %TIME%] LanSchool Student install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log
+)
 GOTO :END
 
 :TEACHER
-If not exist "C:\Program Files (x86)\LanSchool\teacher.exe" msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=%CHANNEL%
+REM Install LanSchool Teacher if not already installed
+IF NOT EXIST "C:\Program Files (x86)\LanSchool\teacher.exe" (
+	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=%CHANNEL%
+	IF %ERRORLEVEL% NEQ 0 echo [%DATE% %TIME%] LanSchool Teacher install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log
+)
 :END
