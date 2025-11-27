e4dd6371f0c5e97569149326fa820bfb766358d0
j1r-admin <john.ralph@bingleygrammar.org>
2025-11-27 10:41:21 +0000

    Add LCS parameter to installation commands for Student and Teacher

--- LanSchool.bat
+++ LanSchool.bat
@@ -11,7 +11,7 @@ IF "%COMPUTERNAME%" == "%TEACHERMC%" GOTO :TEACHER
 :STUDENT
 REM Install LanSchool Student if not already installed
 IF NOT EXIST "C:\Program Files (x86)\LanSchool\student.exe" (
-	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Student.msi" /qn ADVANCED_OPTIONS=1 STEALTH_MODE=1 NO_KEYBOARD_MONITORING_MODE=1 NO_INTERNET_MONITORING_MODE=1 CHANNEL=%CHANNEL%
+	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Student.msi" /qn ADVANCED_OPTIONS=1 STEALTH_MODE=1 NO_KEYBOARD_MONITORING_MODE=1 NO_INTERNET_MONITORING_MODE=1 CHANNEL=%CHANNEL% LCS=192.168.254.20
 	IF %ERRORLEVEL% NEQ 0 echo [%DATE% %TIME%] LanSchool Student install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log
 )
 GOTO :END
@@ -19,7 +19,7 @@ GOTO :END
 :TEACHER
 REM Install LanSchool Teacher if not already installed
 IF NOT EXIST "C:\Program Files (x86)\LanSchool\teacher.exe" (
-	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=%CHANNEL%
+	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=%CHANNEL% LCS=192.168.254.20
 	IF %ERRORLEVEL% NEQ 0 echo [%DATE% %TIME%] LanSchool Teacher install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log
 )
 :END
