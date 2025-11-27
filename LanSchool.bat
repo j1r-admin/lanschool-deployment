561e275abd3c5d7d896e46f16ef69fd318d67c49
j1r-admin <john.ralph@bingleygrammar.org>
2025-11-27 10:52:46 +0000

    Improve logging for LanSchool installation success and failure

--- LanSchool.bat
+++ LanSchool.bat
@@ -12,7 +12,11 @@ IF "%COMPUTERNAME%" == "%TEACHERMC%" GOTO :TEACHER
 REM Install LanSchool Student if not already installed
 IF NOT EXIST "C:\Program Files (x86)\LanSchool\student.exe" (
 	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Student.msi" /qn ADVANCED_OPTIONS=1 STEALTH_MODE=1 NO_KEYBOARD_MONITORING_MODE=1 NO_INTERNET_MONITORING_MODE=1 CHANNEL=%CHANNEL% LCS=192.168.254.20
-	IF %ERRORLEVEL% NEQ 0 echo [%DATE% %TIME%] LanSchool Student install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log
+	IF %ERRORLEVEL% EQU 0 (
+		echo [%DATE% %TIME%] LanSchool Student install succeeded on %COMPUTERNAME% >> C:\LanSchoolInstall.log 2>nul
+	) ELSE (
+		echo [%DATE% %TIME%] LanSchool Student install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log 2>nul
+	)
 )
 GOTO :END
 
@@ -20,6 +24,10 @@ GOTO :END
 REM Install LanSchool Teacher if not already installed
 IF NOT EXIST "C:\Program Files (x86)\LanSchool\teacher.exe" (
 	msiexec.exe /i "\\sccm2\Deployment\Software\LanSchool\latest\Teacher.msi" /qn ADVANCED_OPTIONS=1 CHANNEL=%CHANNEL% LCS=192.168.254.20
-	IF %ERRORLEVEL% NEQ 0 echo [%DATE% %TIME%] LanSchool Teacher install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log
+	IF %ERRORLEVEL% EQU 0 (
+		echo [%DATE% %TIME%] LanSchool Teacher install succeeded on %COMPUTERNAME% >> C:\LanSchoolInstall.log 2>nul
+	) ELSE (
+		echo [%DATE% %TIME%] LanSchool Teacher install failed on %COMPUTERNAME% >> C:\LanSchoolInstall.log 2>nul
+	)
 )
 :END
